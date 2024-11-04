c7b67820f2c4f681bd5a25ca1152e9420a1b956c
lsh <3040114965@qq.com>
2024-11-04 18:29:15 +0800

    简单发送与接受

---
+++ publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -0,0 +1,23 @@
+package com.itheima.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+
+@SpringBootTest
+public class SpringAmqpTest {
+
+    @Resource
+    RabbitTemplate rabbitTemplate;
+
+    @Test
+    // 简单消息的监听
+    public void testSimpleQueue() {
+        String queueName = "simple.queue";
+        String message = "hello, spring amqp!";
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+}
